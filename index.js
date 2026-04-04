0959e90930d71c4f2f8546c88cee73f87b851c83
Gio-Midonz-Bots <gmidonz09@gmail.com>
2026-04-04 17:37:39 -0300

    Objetos

---
+++ index.js
@@ -0,0 +1,48 @@
+const player1 ={
+    Nome: "Mario",
+    Velociade: 4,
+    Manobrabilidade:3,
+    Poder:3,
+    Pontos: 0
+};
+
+const player2 ={
+    Nome: "Bowser",
+    Velociade: 5,
+    Manobrabilidade:2,
+    Poder:5,
+    Pontos: 0
+};
+
+const player3 ={
+    Nome: "Peach",
+    Velociade: 3,
+    Manobrabilidade:4,
+    Poder:2,
+    Pontos: 0
+};
+
+const player4 ={
+    Nome: "Luigi",
+    Velociade: 3,
+    Manobrabilidade:4,
+    Poder:4,
+    Pontos: 0
+};
+
+const player5 ={
+    Nome: "Yoshi",
+    Velociade: 2,
+    Manobrabilidade:4,
+    Poder:3,
+    Pontos: 0
+};
+
+const player6 ={
+    Nome: "Donkey kong",
+    Velociade: 2,
+    Manobrabilidade:2,
+    Poder:5,
+    Pontos: 0
+};
+
